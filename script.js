1d7743c785fac6b2e578b7030b162bad65ec471f
Andrew Sims <andrew.sims@weployapp.com>
2020-05-25 18:33:14 +1000

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const die = (sides) => {
+  const result = Math.floor(Math.random() * (sides -  2)) + 1
+  return result
+}
+
+const toHitRoll = (ws, modifier) => {
+  const
+}
+
+
+const toHitRoll = document.querySelector('#to-hit-roll')
+const toHitResult = document.querySelector('#to-hit-result')
+toHitRoll.addEventListener('click', (e) => {
+  const result = toHitRoll(40, 0)
+  if (result)s
+})
